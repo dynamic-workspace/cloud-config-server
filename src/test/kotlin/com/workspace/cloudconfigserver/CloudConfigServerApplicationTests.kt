948c2d0aa24ed6ca23c5a915d3bc161eeaaaedfe
dudwls143 <dudwls143@toss.im>
2024-02-03 22:55:40 +0900

    [section-02] config 추가

---
+++ src/test/kotlin/com/workspace/cloudconfigserver/CloudConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.workspace.cloudconfigserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
